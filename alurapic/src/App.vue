3b217f9bc112e048d53fa9f1614f82960a2878c9
Jeferson Urbieta da Silva Neto <jeferson.neto@azi.com.br>
2018-03-02 16:37:51 -0400

    first commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app" class="container">
+        <h1>{{ msg }}</h1>
+        <ul v-for="foto in fotos">
+            <li>
+                <img :src="foto.url" :alt="foto.titulo">
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+                fotos: []
+            }
+        },
+        created(){
+            this.$http.get('http://localhost:3000/v1/fotos')
+                .then(res => res.json())
+                .then(fotos => this.fotos = fotos, err => console.log(err));
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ alurapic/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueResource  from 'vue-resource'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue);
+Vue.use(VueResource);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
